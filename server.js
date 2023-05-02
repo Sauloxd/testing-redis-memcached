f958af20e267d152f18b8340aed0179c8a307dc4
saulo furuta <saulo.furuta@qulture.rocks>
2023-05-02 15:07:05 -0300

    add bennchmarks

--- server.js
+++ server.js
@@ -6,32 +6,8 @@ const { chargeRequestMemcached, resetMemcached } = require('./engines/memcached'
 const app = express();
 app.use(express.json());
 
-// fetch("http://localhost:3000/redis/charge_request", {
-//   "headers": {
-//     "accept": "*/*",
-//     "accept-language": "pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7",
-//     "sec-ch-ua": "\"Chromium\";v=\"112\", \"Google Chrome\";v=\"112\", \"Not:A-Brand\";v=\"99\"",
-//     "sec-ch-ua-mobile": "?0",
-//     "sec-ch-ua-platform": "\"macOS\"",
-//     "sec-fetch-dest": "empty",
-//     "sec-fetch-mode": "cors",
-//     "sec-fetch-site": "same-origin",
-//     "Content-Type": "application/json"
-//   },
-//   "referrer": "http://localhost:3000/redis/charge_request_single",
-//   "referrerPolicy": "strict-origin-when-cross-origin",
-//   "body": JSON.stringify({
-//       "serviceType": "voice",
-//       "unit": 2
-//     }),
-//   "method": "POST",
-//   "mode": "cors",
-//   "credentials": "omit"
-// });
-
 app.post('/redis/charge_request', async (req, res, next) => {
   try {
-    console.log(req.body)
     res.json(await chargeRequestRedis(req.body));
   } catch (err) {
     next(err);
@@ -63,7 +39,6 @@ app.post('/memcached/charge_request', async (req, res, next) => {
 
 app.get('/memcached/reset', async (req, res, next) => {
   try {
-    console.log('memcached')
     res.json(await resetMemcached(req.body));
   } catch (err) {
     next(err);
